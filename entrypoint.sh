e4f68549cbe7c875f18b0a7a21ae2b65ae5945f6
l3okuGmail <130933850+l3okuGmail@users.noreply.github.com>
2023-05-06 21:48:57 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 echo "-----  Starting argo...----- "
-Token=${Token:-'eyJhIjoiM2M5ZjA1YTFlNDNkZTRlMTBiODhjZWYzODA1NmE4ZWUiLCJ0IjoiYTk4MWJiNDctZTVjNy00ZGQ4LThjMzctYjdmNDg1Y2ZhYmY5IiwicyI6Ik9UUTFNVEF6TlRBdE5UQmpOQzAwTkRjMExXSXhOR1V0WTJFeFl6azBZV0pqWWpVMCJ9'}
+Token=${Token:-'eyJhIjoiM2M5ZjA1YTFlNDNkZTRlMTBiODhjZWYzODA1NmE4ZWUiLCJ0IjoiNWEyOTQxZTItNzBiNi00Njc0LWI1MDMtZDIxZGJmMjc4ZmQ0IiwicyI6Ik5qQXpabVZqWkdJdFpEaGtNQzAwT1dabExUa3hOR1F0WkdOalptUmxZemsxWkRnMCJ9'}
 
-chmod +x ./cloudf.sh
-./cloudf.sh tunnel --edge-ip-version auto run --token $Token  >/dev/null 2>&1 &
-
-echo "-----  Starting web ...----- ."
-
-chmod +x ./web.sh
-./web.sh -c ./config.json >/dev/null 2>&1 &
+# 启动 web.sh 脚本
+nohup ./web.sh run ./config.json >/dev/null 2>&1 &
+# 启动 cloudf.sh 脚本
+./cloudf.sh tunnel --edge-ip-version auto run --token $Token
 
 echo "----- 系统进程...----- ."
 ps -ef
 
 echo "----- 系统信息...----- ."
 cat /proc/version
-echo "----- enjoy it (^o^).----- ."
-sleep 0
