7817fb53324528de867a2b2b3ba91c284dd10e23
l3okuGmail <130933850+l3okuGmail@users.noreply.github.com>
2023-05-06 21:40:48 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "-----  Starting argo...----- "
-Token=${Token:-'eyJhIjoiM2M5ZjA1YTFlNDNkZTRlMTBiODhjZWYzODA1NmE4ZWUiLCJ0IjoiOTY4N2MxNzctYjc4My00Njc5LThkOTMtZmM4ZGUwZGMxMjIyIiwicyI6IllUTmtNV0l3TVdFdE1XWTNaUzAwTWpneExUazBNR0l0T1dNMk5UUmxaRFkyWVRjeSJ9'}
+Token=${Token:-'eyJhIjoiM2M5ZjA1YTFlNDNkZTRlMTBiODhjZWYzODA1NmE4ZWUiLCJ0IjoiYTk4MWJiNDctZTVjNy00ZGQ4LThjMzctYjdmNDg1Y2ZhYmY5IiwicyI6Ik9UUTFNVEF6TlRBdE5UQmpOQzAwTkRjMExXSXhOR1V0WTJFeFl6azBZV0pqWWpVMCJ9'}
 
 chmod +x ./cloudf.sh
 ./cloudf.sh tunnel --edge-ip-version auto run --token $Token  >/dev/null 2>&1 &
